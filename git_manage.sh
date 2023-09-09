8b94b73afe4fe528f0e0796bce3060ba496c5d22
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-09 10:41:19 +0300

    set up some basic functions

--- git_manage.sh
+++ git_manage.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+Introduction (){
+    echo "Git-manage, an easy to use and lightweight tool for managing multiple git repositories."
+    echo -e "\nWhat would you like to do?"
+    echo "1-Run a command on your desired set of repos."
+    echo "2-Add to saved repos."
+    echo "2-View saved repos."
+    echo "3-Delete saved repos."
+}
 
-echo "Git-manage, an easy to use and lightweight tool for managing multiple git repositories."
-echo -e "\nWhat would you like to do?"
-echo "1-Run a command on your desired set of repos."
-echo "2-Modify, view or delete your set of saved repos"
+execute_in_repos (){
+    
+}
+
+add_to_repos (){
+
+}
+
+delete_saved_repos (){
+    
+}
