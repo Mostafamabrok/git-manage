dd125d4837a405ab0fdfe853e54446b2dfcf1d24
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-09 10:16:47 +0300

    Added some string prompts.

--- git_manage.sh
+++ git_manage.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "git-manage, an easy to use and lightweight tool for managing multiple git repositories."
+echo "Git-manage, an easy to use and lightweight tool for managing multiple git repositories."
+echo -e "\nWhat would you like to do?"
+echo "1-Run a command on your desired set of repos."
+echo "2-Modify, view or delete your set of saved repos"
