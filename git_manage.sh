9cdda8e7fd79fec91b8191ca69a4caa861816065
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-10 05:40:29 +0300

    Added some pseudo code for later.

--- git_manage.sh
+++ git_manage.sh
@@ -3,19 +3,22 @@ Introduction (){
     echo "Git-manage, an easy to use and lightweight tool for managing multiple git repositories."
     echo -e "\nWhat would you like to do?"
     echo "1-Run a command on your desired set of repos."
-    echo "2-Add to saved repos."
-    echo "2-View saved repos."
-    echo "3-Delete saved repos."
+    echo "2-Add to saved repo list."
+    echo "2-View saved repo list."
+    echo "3-Delete saved repo list."
 }
 
 execute_in_repos (){
-    
+    #Ask what command to execute
+    #For each line in savedrepos, change directory to that line and execute the command.
 }
 
 add_to_repos (){
-
+    #Ask for path to repo
+    #Append it to saved repos
 }
 
 delete_saved_repos (){
-    
+    #Make sure the user wants to delete their list of savedrepos
+    #Delte savedrepos.txt
 }
