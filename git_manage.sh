c62d5e853404a97e7b1e22a4fad1484e27d9235c
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-10 12:09:43 +0300

    Made `add_to_repos()` function work.

--- git_manage.sh
+++ git_manage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Introduction (){
+Introduction () {
     echo "Git-manage, an easy to use and lightweight tool for managing multiple git repositories."
     echo -e "\nWhat would you like to do?"
     echo "1-Run a command on your desired set of repos."
@@ -8,17 +8,19 @@ Introduction (){
     echo "3-Delete saved repo list."
 }
 
-execute_in_repos (){
+execute_in_repos () {
     #Ask what command to execute
     #For each line in savedrepos, change directory to that line and execute the command.
+    echo "prevents runtime bug"
 }
 
-add_to_repos (){
-    #Ask for path to repo
-    #Append it to saved repos
+add_to_repos () {
+    read -p "Enter the full path to your repo." added_repo
+    echo "$added_repo" >> SavedRepos.txt
 }
 
-delete_saved_repos (){
+delete_saved_repos () {
     #Make sure the user wants to delete their list of savedrepos
     #Delte savedrepos.txt
+    echo "prevents runtime bug"
 }
